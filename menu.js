9f738f159f401bfc7c9634fe0dde480e39bada40
Samaan Ghani <samaanghani@gmail.com>
2013-01-29 17:12:04 -0500

    hopefully final screens

--- menu.js
+++ menu.js
@@ -2,43 +2,15 @@ Welcome = function () {
 	canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     var menu = new Image();
-    var play = new Image();
-    var help = new Image();
-    var highlight = false;
-	menu.src = "menu.png";
-	play.src = "playbutton.png";
-	help.src = "helpbutton.png";
+	menu.src = "Menu.png";
 	var drawMainMenu = function(){
 
 		ctx.drawImage(menu, 0, 0);  // draw image at (0, 0)
-		//false is play true is help
-		if (highlight === false){
-			ctx.fillStyle = "#FFFFFF";
-			ctx.fillRect(640, 490, 130, 60);
-		}
-		else{
-			ctx.fillStyle = "#FFFFFF";
-			ctx.fillRect(790,490,130,60);
-		}
-		ctx.drawImage(play, 650,495,110,50);
-		ctx.drawImage(help, 800,495,110,50);
 	}
 
 	var onKeyPress = function(event){
-    	if (event.keyCode === 39){
-    		highlight = true;
-    	}
-    	else if(event.keyCode === 37){
-    		highlight = false;
-    	}
-    	else if(highlight === false && event.keyCode === 13){
-    		window.clearInterval(drawMain);
-    		canvas.removeEventListener('keydown',onKeyPress,false);
-    		App();
-    		return;
-    	}
-    	else if(highlight === true && event.keyCode === 13){
-    		window.clearInterval(drawMain);
+    	if (event.keyCode === 13){
+			window.clearInterval(drawMain);
     		canvas.removeEventListener('keydown', onKeyPress, false);
     		Help();
     		return;
@@ -54,46 +26,16 @@ Welcome = function () {
 
 Help = function (){
 	var help = new Image();
-	var back = new Image();
-	var more = new Image();
-	var helphighlight = false;
-	help.src = "helpMenu1.png";
-	back.src = "backbutton.png";
-	more.src = "morebutton.png";
+	help.src = "help.png";
 	var drawHelpMenu = function(){
 		ctx.drawImage(help, 0, 0);  // draw image at (0, 0)
-		if (helphighlight === false){
-			ctx.fillStyle = "#FFFFFF";
-			ctx.fillRect(640, 490, 130, 60);
-		}
-		else{
-			ctx.fillStyle = "#FFFFFF";
-			ctx.fillRect(790,490,130,60);
-		}
-		ctx.drawImage(more, 650,495,110,50);
-		ctx.drawImage(back, 800,495,110,50);
-
 	}
 
 	var onKeyPressHelp = function(event){
-		if (event.keyCode === 39){
-    		helphighlight = true;
+		if (event.keyCode === 13){
+    		alert("show tutorial");
     	}
-    	else if(event.keyCode === 37){
-    		helphighlight = false;
-    	}
-		else if (event.keyCode ===13 && helphighlight === false){
-			alert("show tutorial");
-		}
-		else if(event.keyCode ==13 && helphighlight == true){
-			window.clearInterval(drawHelp);
-			canvas.removeEventListener('keydown', onKeyPressHelp,false);
-			Welcome();
-			return;
-		}
-
-}
-
+	}
 	canvas.addEventListener('keydown', onKeyPressHelp, false);
 	var drawHelp = setInterval(drawHelpMenu, 1000/20);
 }
